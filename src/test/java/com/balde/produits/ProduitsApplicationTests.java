815cbcfcde0b01a8fa14acfc2b0d49fad90088db
Mamadou Oury <ouryboulloye@gmail.com>
2024-12-04 08:46:14 +0100

    insertion d'un nouveau produit

--- src/test/java/com/balde/produits/ProduitsApplicationTests.java
+++ src/test/java/com/balde/produits/ProduitsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.balde.produits;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.balde.produits.entities.Produit;
+import com.balde.produits.repos.ProduitRepository;
 
 @SpringBootTest
 class ProduitsApplicationTests {
 
+	@Autowired
+	public ProduitRepository produitRepository;
+	
 	@Test
-	void contextLoads() {
+	public void testCreateProduit() {
+		Produit prod = new Produit("PC Dell", 2200.500, new Date());
+		produitRepository.save(prod);
 	}
-
 }
